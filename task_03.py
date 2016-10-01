61edf5bc344963cde25e81d9f6afa2d5afb0913a
Sunil Prajapati <nrk_619@yahoo.com>
2016-10-01 21:14:03 +0000

    Tuples replaced

---
+++ task_03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Practicing adding and deleting in tupls"""
+
+import data
+
+DIRECTIONS = data.DIRECTIONS
+
+DIRECTIONS = DIRECTIONS[:-1] + ('West',)
