79946600f155f1fabfdf9e23281cf7232bfc6e7d
Sunil Prajapati <nrk_619@yahoo.com>
2016-10-01 01:59:57 +0000

    List and tuple created

---
+++ task_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Creating lists and duple"""
+
+ELEMENTS = 'None' , 'Hydrogen' , 'Helum','Lithium','Beryllium','Boron','Carbon'
+
+OPERATIONS_ORDER = ['Parenthesis','Exponents','Multiplication','Division','Addition','Subtraction']
