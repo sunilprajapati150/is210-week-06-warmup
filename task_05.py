5d709685d5d7a03b5e2427ca78ad40976374f177
Sunil Prajapati <nrk_619@yahoo.com>
2016-10-02 01:37:31 +0000

    commited

---
+++ task_05.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""showing difference between tuple and list"""
+
+def flip_keys(to_flip):
+    count = 0
+    for seq in to_flip():
+        seq[::-1]
+        counter += 1
+        return to_flip
+        
